8ffe7b76a09a7c512d451a0117fc7ecc732c8c45
Abdoulaye DIA <abdoudu78130@gmail.com>
2023-06-11 17:45:46 +0200

    Updated the https://github.com/andrewrk/sdl-zig-demo
    - to 0.11-dev
    - Little square moving around with arrow keys
    - Some text (thus the Roboto Google Font)

--- build.zig
+++ build.zig
@@ -1,16 +1,43 @@
-const Builder = @import("std").build.Builder;
+const std = @import("std");
 
-pub fn build(b: *Builder) void {
-    const mode = b.standardReleaseOptions();
-    const exe = b.addExecutable("sdl-zig-demo", "src/main.zig");
-    exe.setBuildMode(mode);
+// Although this function looks imperative, note that its job is to
+// declaratively construct a build graph that will be executed by an external
+// runner.
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "ZDL2",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+
     exe.linkSystemLibrary("SDL2");
     exe.linkSystemLibrary("c");
 
-    b.default_step.dependOn(&exe.step);
     b.installArtifact(exe);
 
-    const run = b.step("run", "Run the demo");
-    const run_cmd = exe.run();
-    run.dependOn(&run_cmd.step);
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    const unit_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_unit_tests.step);
 }
+
--- src/main.zig
+++ src/main.zig
@@ -1,63 +1,123 @@
 const c = @cImport({
     @cInclude("SDL2/SDL.h");
 });
+
+const std = @import("std");
 const assert = @import("std").debug.assert;
+pub fn lerp(a: f32, b: f32, t: f32) f32 {
+    return a + (b - a) * t;
+}
 
 pub fn main() !void {
+    // Initialize SDL and prepare for awesomeness!
     if (c.SDL_Init(c.SDL_INIT_VIDEO) != 0) {
         c.SDL_Log("Unable to initialize SDL: %s", c.SDL_GetError());
         return error.SDLInitializationFailed;
     }
     defer c.SDL_Quit();
 
-    const screen = c.SDL_CreateWindow("My Game Window", c.SDL_WINDOWPOS_UNDEFINED, c.SDL_WINDOWPOS_UNDEFINED, 300, 73, c.SDL_WINDOW_OPENGL) orelse
+    // Create the game window where the magic will happen
+    const screen = c.SDL_CreateWindow("ZigSDL2", c.SDL_WINDOWPOS_UNDEFINED, c.SDL_WINDOWPOS_UNDEFINED, 720, 480, c.SDL_WINDOW_OPENGL) orelse
         {
         c.SDL_Log("Unable to create window: %s", c.SDL_GetError());
         return error.SDLInitializationFailed;
     };
     defer c.SDL_DestroyWindow(screen);
 
+    // Create the renderer to draw our glorious creations
     const renderer = c.SDL_CreateRenderer(screen, -1, 0) orelse {
         c.SDL_Log("Unable to create renderer: %s", c.SDL_GetError());
         return error.SDLInitializationFailed;
     };
     defer c.SDL_DestroyRenderer(renderer);
 
-    const zig_bmp = @embedFile("zig.bmp");
-    const rw = c.SDL_RWFromConstMem(zig_bmp, zig_bmp.len) orelse {
-        c.SDL_Log("Unable to get RWFromConstMem: %s", c.SDL_GetError());
-        return error.SDLInitializationFailed;
-    };
-    defer assert(c.SDL_RWclose(rw) == 0);
-
-    const zig_surface = c.SDL_LoadBMP_RW(rw, 0) orelse {
-        c.SDL_Log("Unable to load bmp: %s", c.SDL_GetError());
-        return error.SDLInitializationFailed;
-    };
-    defer c.SDL_FreeSurface(zig_surface);
-
-    const zig_texture = c.SDL_CreateTextureFromSurface(renderer, zig_surface) orelse {
-        c.SDL_Log("Unable to create texture from surface: %s", c.SDL_GetError());
-        return error.SDLInitializationFailed;
-    };
-    defer c.SDL_DestroyTexture(zig_texture);
-
+    // Prepare the stage for our main actors (just a red square)
+    // update: I could've at least set the square color to match the zig logo
+    // update: I did it.
+    // update: I undid it.
+    // update: I did it again.
     var quit = false;
+    const SquareSize = 50;
+    var square_x: i32 = 0;
+    var square_y: i32 = 0;
+    var walk_speed: f32 = 200; // Yes, It is a cube but imagine it has legs....
+    var sonic_speed: f32 = 500; // Sonic Solos Attack on Titan whole verse, don't @ me
+    var current_speed: f32 = 0;
+    var is_running = false;
+    var last_time = c.SDL_GetTicks();
+
+    const zig_logo_color_im_undecisive_help_me = c.SDL_Color{ .r = 247, .g = 164, .b = 29, .a = 255 };
+
+    // The show begins!
     while (!quit) {
+        // Interact with the audience, respond to their cheers and jeers
         var event: c.SDL_Event = undefined;
         while (c.SDL_PollEvent(&event) != 0) {
-            switch (event.@"type") {
+            switch (event.type) {
                 c.SDL_QUIT => {
-                    quit = true;
+                    quit = true; // The audience wants to leave, let's wrap it up!
                 },
-                else => {},
+                c.SDL_KEYDOWN => {
+                    const key_code = event.key.keysym.sym;
+                    switch (key_code) {
+                        c.SDLK_ESCAPE => {
+                            quit = true; // The audience demands an immediate exit, let's not upset them!
+                        },
+                        c.SDLK_LSHIFT => {
+                            is_running = true; // Activate turbo mode, it's time to go FAST!
+                        },
+                        else => {
+                            std.debug.print("Key: {} PRESSED.\n", .{key_code}); // Unrecognized key, let's pretend we know what it does!
+                        },
+                    }
+                },
+                c.SDL_KEYUP => {
+                    const key_code = event.key.keysym.sym;
+                    if (key_code == c.SDLK_LSHIFT) {
+                        is_running = false; // Slow down, the turbo mode has been disengaged!
+                    }
+                },
+                else => {}, // We're not interested in other events, let's keep the show going!
             }
         }
 
+        // Manage time and motion, it's all about precision!
+        const current_time = c.SDL_GetTicks();
+        const delta_time = current_time - last_time;
+        last_time = current_time;
+        const dt_seconds = @intToFloat(f32, delta_time) / 1000.0; // Those macros are noice!!
+
+        // Determine the appropriate speed, whether it's walking or running
+        const target_speed = if (is_running) sonic_speed else walk_speed;
+        current_speed = lerp(current_speed, target_speed, 0.1);
+        const movement = @floatToInt(i32, current_speed * dt_seconds); // Could've done it another way but hopefully it showcases the macros in a nice way
+
+        const state = c.SDL_GetKeyboardState(null);
+
+        if (state[c.SDL_SCANCODE_LEFT] != 0) {
+            square_x -= movement;
+        }
+        if (state[c.SDL_SCANCODE_RIGHT] != 0) {
+            square_x += movement;
+        }
+        if (state[c.SDL_SCANCODE_UP] != 0) {
+            square_y -= movement;
+        }
+        if (state[c.SDL_SCANCODE_DOWN] != 0) {
+            square_y += movement;
+        }
+
+        // Set the stage, it's time for the grand finale!
+        _ = c.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Black color
         _ = c.SDL_RenderClear(renderer);
-        _ = c.SDL_RenderCopy(renderer, zig_texture, null, null);
+
+        // Present our star performer, the magnificent square!
+        const square_rect = c.SDL_Rect{ .x = square_x, .y = square_y, .w = SquareSize, .h = SquareSize };
+        _ = c.SDL_SetRenderDrawColor(renderer, zig_logo_color_im_undecisive_help_me.r, zig_logo_color_im_undecisive_help_me.g, zig_logo_color_im_undecisive_help_me.b, zig_logo_color_im_undecisive_help_me.a);
+        _ = c.SDL_RenderFillRect(renderer, &square_rect);
+
         c.SDL_RenderPresent(renderer);
 
-        c.SDL_Delay(17);
+        c.SDL_Delay(17); // Pause for a moment, catch your breath, and let the applause fill the air!
     }
 }
